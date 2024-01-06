86111906ecfc0c9ef97e30c4f1d7e9e95620e99b
Laura1608 <131393433+Laura1608@users.noreply.github.com>
2024-01-06 20:54:59 +0100

    Scraping the data of each service provider subpage

--- webscraping-project-afm.py
+++ webscraping-project-afm.py
@@ -1,42 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
+import re
 
-url = 'https://www.afm.nl/nl-nl/sector/registers/vergunningenregisters/financiele-dienstverleners'
+# Creating different versions of the url with regex
+## url = f'https://www.afm.nl/nl-nl/sector/registers/vergunningenregisters/financiele-dienstverleners/details?id={i}'
+## i = re.findall("\w*-\w*-\w*-\w*-\w*", content)
 
 # Create dictionary with user-agent = Firefox
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0'}
 
-# Add user-agent Firefox to url
+# Retrieve content from webpage
 response = requests.get(url, headers=headers)
 content = response.content
+soup = BeautifulSoup(content, 'html.parser')
+
 
 # Scrape and find content based on class
-soup = BeautifulSoup(content, 'html.parser')
-results = soup.find_all('tr', class_='jq_registers_register-paged-list_results_tr')
+list_results = soup.find_all('div', class_='cc-em--detail-list__items')
 
 # Create empty list to store scraped content
-register_list = []
+results_list = []
 
-# While looping over table rows, loop over content in columns, and add it to list
-for tr in results:
-
-    data = []
-    for td in tr.find_all('td'):
-        if td.a:
-            data.append(td.a.text.strip())
-        else:
-            data.append(td.text.strip())
-
-    register_list.append(data)
+# Loop over list, save content from every column, and add it to the final list
+for result in list_results:
+    for span in result.find_all('span', class_='cc-em--detail-list__value'):
+        results_list.append(span.text.strip())
 
 
-# Save list data to csv file
-with open('Register_Service_Providers.csv', 'w', newline='', encoding="utf-8") as file:
-    headers = ["Statutaire_naam", "Handelsnaam", "Vestigingsplaats"]
-    write = csv.writer(file)
+# Scrape and find content based on class
+table_results = soup.find_all('table', class_='cc-table-wrap-text')
 
-    write.writerow(headers)
-    write.writerows(register_list)
+# Create empty list to store scraped content
+results_table = []
 
-print("Data extraction completed")
+# Loop over table, get the text content following the span element within the table data, and add it to the final list
+for result in table_results:
+    for span in result.find_all('span', class_='cc-mobile-title'):
+        text_content = span.next_sibling
+        results_table.append(text_content)
+
+
+''' TO DO:
+*Combine data in 1 list instead of 2
+*Test regex
+*Save as csv?
+'''
