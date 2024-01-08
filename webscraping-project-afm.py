98e46271f09c34e74273f51403b38cc9c74c50bb
Laura1608 <131393433+Laura1608@users.noreply.github.com>
2024-01-08 14:38:12 +0100

    Working on receiving table data per row

--- webscraping-project-afm.py
+++ webscraping-project-afm.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import csv
+import os.path
 
 # Creating different versions of the url with regex
 # url = f'https://www.afm.nl/nl-nl/sector/registers/vergunningenregisters/financiele-dienstverleners/details?id={i}'
@@ -21,40 +23,47 @@ soup = BeautifulSoup(content, 'html.parser')
 # Scrape and find content based on class
 list_results = soup.find_all('div', class_='cc-em--detail-list__items')
 
-# Create empty list to store scraped content
-results_list = []
+# Create empty list to store scraped content with company info
+register = []
 
 # Loop over list, save content from every column, and add it to the final list
 for result in list_results:
     for span in result.find_all('span', class_='cc-em--detail-list__value'):
-        results_list.append(span.text.strip())
+        register.append(span.text.strip())
 
 
-# Scrape and find content based on class
-table_results = soup.find_all('table', class_='cc-table-wrap-text')
-
-# Create empty list to store scraped content
-results_table = []
+# Scrape and find table content
+table_results = soup.find_all('tbody')
 
 # Loop over table, get the text content following the span element within the table data, and add it to the final list
 for tr in table_results:
-    data = []
     for td in tr.find_all('td'):
         for span in td.find_all('span', class_='cc-mobile-title'):
             # Extract the text content after the span element within the td
             text_content = span.next_sibling
-            data.append(text_content)
-    results_table.append(data)
+            register.append(text_content)
 
-print(results_table)
 #
-# for row in results_table:
-#     company_info = results_list + results_table
+# # Before saving the file, check if already exists
+# file_exists = os.path.isfile("Register_AFM_Output.csv")
 #
-# print(company_info)
+# # Create new csv file to add data to
+# with open("Register_AFM_Output.csv", "a", newline='') as file:
+#     headers = ["Statutaire_naam", "Handelsnaam", "Vergunningnummer", "Adres", "Land", "KvK", "Financiele_dienst", "Product", "Begindatum", "Einddatum"]
+#
+#     writer = csv.DictWriter(file, delimiter=',', fieldnames=headers, extrasaction='ignore', dialect='unix')
+#
+#     # Only add headers when the file is newly created
+#     if not file_exists:
+#         writer.writeheader()
+#
+#     # Loop over every result on business page
+#     for item in company_info:
+#         writer.writerow(item)
+#
+# print('Company information retrieved!')
 
-''' TO DO:
-*Combine data in 1 list instead of 2
-*Test regex
-*Save as csv?
-'''
+# TO DO:
+# Get each tr on new row
+# Make csv save work
+# Make regex work
